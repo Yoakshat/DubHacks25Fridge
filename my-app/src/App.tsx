4f4a7322a31a49938eae8c7ab44e6b6527835645
Yoakshat <51726869+Yoakshat@users.noreply.github.com>
2025-10-18 16:14:17 -0700

    Add signup feature

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Home from './pages/Home'
 import Alice from './pages/Alice'
+import Signup from './pages/Signup'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/alice" element={<Alice />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </main>
     </BrowserRouter>
---
+++ my-app/src/pages/Signup.tsx
@@ -0,0 +1,11 @@
+export default function Signup(){
+    return (
+        <div> 
+            <h3>Username</h3> 
+            <input type="text" id="username" placeholder="Type your username here"/>
+
+            <h3>Password</h3> 
+            <input type="text" id="password" placeholder="Type your password here"/>
+        </div> 
+    )
+}
