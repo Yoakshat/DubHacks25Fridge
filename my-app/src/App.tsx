8919d7e129c64000d0acd953da9564ee1275cde1
jena986 <jena986@students.cs.ubc.ca>
2025-10-18 17:08:38 -0700

    created KidsCreation.tsx file

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Home from './pages/Home'
 import Alice from './pages/Alice'
+import KidsCreation from './pages/KidsCreation'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/alice" element={<Alice />} />
+          <Route path="/kidsCreation" element={<KidsCreation />} />
         </Routes>
       </main>
     </BrowserRouter>
---
+++ my-app/src/pages/KidsCreation.tsx
@@ -0,0 +1,8 @@
+export default function KidsCreation() {
+    return (
+        <div>
+          <h1>Home</h1>
+          <p>hi im jena</p>
+        </div>
+      )
+}
