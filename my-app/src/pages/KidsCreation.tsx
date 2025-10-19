e3de979318d06d6ebfd2ac3ce0f9bea8b6d9da6e
jena986 <jena986@students.cs.ubc.ca>
2025-10-18 17:12:28 -0700

    testing

--- my-app/src/pages/KidsCreation.tsx
+++ my-app/src/pages/KidsCreation.tsx
@@ -1,7 +1,7 @@
 export default function KidsCreation() {
     return (
         <div>
-          <h1>Home</h1>
+          <h1>Jena's testing page</h1>
           <p>hi im jena</p>
         </div>
       )
